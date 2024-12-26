0b99217c3242762c6b536c73f6d96581243b6082
sagar181203 <Sagar181203@gmail.com>
2024-12-27 01:10:20 +0530

    bash

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Install system dependencies for matplotlib
+apt-get update
+apt-get install -y python3-dev python3-pip python3-setuptools
+apt-get install -y libfreetype6-dev pkg-config
+
+# Create directories and copy files
 mkdir -p dist
 cp -r templates/* dist/
 cp -r static dist/
